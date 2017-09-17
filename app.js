e3ee7cf1dcf7602174096e324decb79e1aa70cad
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-09-17 21:36:44 +0530

    Cleaning up

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
-
-
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}, You are ${notes.age}.`);
