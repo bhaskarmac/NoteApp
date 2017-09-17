d783fa94c8178cf363b19488341229c7e0328fcc
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-09-17 19:29:59 +0530

    Adding username to file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log('Starting app');
 
 const fs = require('fs');
+const os = require('os');
 
+var user = os.userInfo();
 
-fs.appendFile('greetings.txt', 'Hello world');
+fs.appendFile('greetings.txt', 'Hello '+ user.username + '!');
