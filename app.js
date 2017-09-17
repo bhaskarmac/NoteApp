9e98ce356c1195f2e8a72be444f10e0dad65e76a
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-09-17 21:25:29 +0530

    Adding addNote method
    Adding package.json file

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', 'Hello '+ user.username + '!');
+fs.appendFile('greetings.txt', `Hello ${user.username}, You are ${notes.age}.`);
