e666f2c1f497be9d84bb04bdcd57eaba5a7679d0
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-09-17 21:28:39 +0530

    Adding lodash module

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
 
 var user = os.userInfo();
 
