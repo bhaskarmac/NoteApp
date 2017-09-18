bb0bb81d29c901a942eccea3122f86a3ed382eaf
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-09-18 19:38:27 +0530

    Adding different command skeletons

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ console.log('Starting app.js');
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
+
+
+var command = process.argv[2];
+console.log('command=>', command);
+
+if(command === 'add'){
+  console.log('adding new note');
+} else if (command === 'list') {
+  console.log('listing all notes');
+} else if (command === 'read') {
+  console.log('reading note');
+} else if (command === 'remove') {
+  console.log('removing note');
+} else {
+  console.log('command not recognised');
+}
